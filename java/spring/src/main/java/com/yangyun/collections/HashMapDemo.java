2c930d7c2d68abd1950b323e500e34e04e75d4bc
yangyun_glb <yangyun_glb@.com>
2019-07-22 11:47:49 +0800

    2019年7月22日11:47:43 杨云 hash 冲突解决的几种方式

--- java/spring/src/main/java/com/yangyun/collections/HashMapDemo.java
+++ java/spring/src/main/java/com/yangyun/collections/HashMapDemo.java
@@ -8,13 +8,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class HashMapDemo {
 
     private static final String STR = "Lq9BH,MR8aH,MQXAg,MQXBH,MR9Ag,N1wAg,N1wBH,N2WaH,LpXBH,N38aH,N39BH,N39Ag,N2XBH,LowAg,LpXAg,LpWaH,LowBH,Lq9Ag,MPwAg,MPvaH,MPwBH";
 
     public static void main(String[] args) {
-        Map<String, String> map = new HashMap<>();
+        Map<String, String> map = new ConcurrentHashMap<>();
 //        String[] split = STR.split(",");
 //        for (int i = 0; i < split.length; i++){
 //            map.put(split[i], split[i]);
@@ -23,7 +24,5 @@ public class HashMapDemo {
 
         String a = map.remove("a");
         System.out.println(a);
-
-
     }
 }
